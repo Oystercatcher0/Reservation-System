94b395136bb8fd4629c0929175144d4c2b187427
Paulina <paulina.matulionyte@gmail.com>
2022-08-18 18:30:00 +0300

    Initial commit

---
+++ src/main/java/lt/codeacademy/RSystemApplication.java
@@ -0,0 +1,13 @@
+package lt.codeacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RSystemApplication.class, args);
+	}
+
+}
